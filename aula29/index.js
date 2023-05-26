af02d423fa32de028333f630a70c3951c3aa576b
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-05-26 14:53:46 -0300

    Update aula29

--- aula29/index.js
+++ aula29/index.js
@@ -1,9 +1,42 @@
-const data = new Date('1987-04-19 00:00:00');
+function getDiaSemanaTexto (diaSemana) {
+    
+    let diaSemanaTexto;
 
+    switch (diaSemana) {
+case 0: 
+    diaSemanaTexto = 'Domingo';
+    return diaSemanaTexto;
+case 1: 
+    diaSemanaTexto = 'Segunda';
+    return diaSemanaTexto;
+case 2: 
+    diaSemanaTexto = 'Terça';
+    return diaSemanaTexto;
+case 3: 
+    diaSemanaTexto = 'Quarta';
+    return diaSemanaTexto;
+case 4: 
+    diaSemanaTexto = 'Quinta';
+    return diaSemanaTexto;
+case 5: 
+    diaSemanaTexto = 'Sexta';
+    return diaSemanaTexto;
+case 6: 
+    diaSemanaTexto = 'Sábado';
+    return diaSemanaTexto;
+default:
+    diaSemanaTexto = '';
+    return diaSemanaTexto;
+} 
+}
+
+const data = new Date('1987-04-20 00:00:00');
 let diaSemana = data.getDay();
-diaSemana = 7;
-let diaSemanaTexto;
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
 
+console.log(diaSemana, diaSemanaTexto);
+
+/* 
 switch (diaSemana) {
 case 0: 
     diaSemanaTexto = 'Domingo';
@@ -26,10 +59,11 @@ case 5:
 case 6: 
     diaSemanaTexto = 'Sábado';
     break;
-default 7:
+default:
     diaSemanaTexto = '';
     break;
 } 
+*/
 
 /*
 if (diaSemana === 0) {
@@ -51,4 +85,4 @@ if (diaSemana === 0) {
 } 
 */
 
-console.log(diaSemana, diaSemanaTexto);
+// console.log(diaSemana, diaSemanaTexto);
