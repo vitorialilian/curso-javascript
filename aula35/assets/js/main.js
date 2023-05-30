3ec0bd2c019e104c1cf43208162f5d3bf0aa35e5
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-05-30 16:25:33 -0300

    Update aula35

--- aula35/assets/js/main.js
+++ aula35/assets/js/main.js
@@ -1,8 +1,8 @@
-const elementos = [
-    {tag: 'p', texto: 'Frase 1'},
-    {tag: 'div', texto: 'Frase 2'},
-    {tag: 'footer', texto: 'Frase 3'},
-    {tag: 'section', texto: 'Frase 4'}
+const elementos = [                    // indice
+    {tag: 'p', texto: 'Frase 1'},      // 0
+    {tag: 'div', texto: 'Frase 2'},    // 1
+    {tag: 'footer', texto: 'Frase 3'}, // 2
+    {tag: 'section', texto: 'Frase 4'} // 3
 ];
 
 const container = document.querySelector('.container');
