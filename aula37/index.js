9a708a9c901f27163d3ae4e0831c855a69c44551
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-05-30 19:58:19 -0300

    aula37
    For of - Estrutura de repetição

---
+++ aula37/index.js
@@ -0,0 +1,49 @@
+//            01234567891011
+// const nome = 'Luiz Otávio'
+
+// for (let i = 0; i < nome.length; i++) {
+//     console.log(nome[i]);
+// }
+
+// for (let i in nome) {
+//     console.log(nome[i]);
+// }
+
+// for (let valor of nome) {
+//     console.log(valor);
+// }
+
+//                  0         1          3
+/* const nomes = ['Luiz', 'Otávio', 'Henrique'];
+
+// For of
+for (let valor of nomes) {
+    console.log(valor);
+}
+
+console.log('----');
+
+// For in
+for (let i in nomes) {
+    console.log(nomes[i]);
+}
+
+console.log('----');
+
+// For Clássico
+for (let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i]);
+}
+
+console.log('----');
+
+// ForEach
+nomes.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array);
+}); */
+
+// For clássico - Geralmente com iteráveis (array ou strings)
+
+// For in - Retorna o índice ou chave (string, array ou object)
+
+// For of - Retorna o valor em si (iteráveis, arrays ou strings)
