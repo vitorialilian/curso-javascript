73922813379d5a919e59d0b6bb754b18fe673fbc
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-01 18:04:53 -0300

    aula41
    Exercício com lógica da programação - 01

---
+++ aula41/index.js
@@ -0,0 +1,40 @@
+// Essa função recebe 2 números e retorne o maior deles
+
+let a = 3;
+let b = 1;
+
+function max(a, b) {
+    if (a > b) return a;
+    return b;
+}
+
+let c = max(a,b);
+const resultado = c;
+
+// console.log(resultado);
+
+// Essa função recebe dois números e retorna o maior deles
+
+let d = 3;
+let e = 4;
+
+function min(d, e) {
+    if (d < e) return d;
+    return e;
+}
+
+let f = min(d, e);
+const resultado2 = f;
+
+// console.log(resultado2);
+
+// Escreva uma função que recebe dois números e retorne se são iguais ou diferentes
+const x = resultado;
+const y = resultado2;
+
+function same(x, y) {
+    if (x === y) return 'Iguais';
+    return 'Diferentes';
+}
+
+console.log(`${x} e ${y} são ${same(x, y)}`);
---
+++ aula41/main.js
@@ -0,0 +1,34 @@
+// escreva uma função que recebe 2 números e retorne o maior deles
+
+// Modo 1
+function max(x, y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+console.log(max(15, 5));
+
+// Modo 2
+function max(x, y) {
+    if (x > y) return x;
+    return y;
+}
+
+console.log(max(6, 2));
+
+// Modo 3
+function max(x, y) {
+    return x > y ? x : y;  
+}
+
+console.log(max(10, 6));
+
+// Modo 4
+const max2 = (x, y) => {
+    return x > y ? x : y;
+}
+
+console.log(max2(5, 8));
