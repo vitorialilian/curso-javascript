5c9f5897af8090ebb03b8ac60d56efb901431aa9
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-06 13:14:21 -0300

    Update aula49

--- aula49/index.js
+++ aula49/index.js
@@ -11,7 +11,24 @@ const souUmDado = function() {
 };
 
 function executaFuncao(funcao) {
+    console.log('Vou executar sua função abaixo:');
     funcao();
 }
 
 executaFuncao(souUmDado);
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function');
+}
+funcaoArrow();
+
+// Dentro de um objeto
+
+const obj = {
+    // falar: function() ou 
+    falar() {
+        console.log('Estou falando...');
+    }
+};
+obj.falar();
