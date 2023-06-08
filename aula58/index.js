3add2fdea67a3f370f38e697b452ffbe07d9a762
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-08 17:15:25 -0300

    Update aula58

--- aula58/index.js
+++ aula58/index.js
@@ -1,1 +1,16 @@
-// Função construtora -> obj
+// Função construtora -> objetos
+// Função fabrica -> objetos
+// Construtora -> Pessoa (new)
+
+function Pessoa(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    this.metodo = function() {
+        console.log(this.nome + ': sou um método');
+    };
+}
+
+const p1 = new Pessoa('Luiz', 'Otávio');
+const p2 = new Pessoa('Maria', 'Oliveira');
+p2.metodo();
