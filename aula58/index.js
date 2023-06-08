f389cbfd7ec01aa3ed5a60f258342dc9dc63b63a
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-08 17:24:43 -0300

    Update aula58

--- aula58/index.js
+++ aula58/index.js
@@ -3,6 +3,14 @@
 // Construtora -> Pessoa (new)
 
 function Pessoa(nome, sobrenome) {
+    // Atributos ou métodos privados
+    const ID = 123456;
+
+    const metodoInterno = function() {
+
+    };
+
+    // Atributos ou métodos públicos
     this.nome = nome;
     this.sobrenome = sobrenome;
 
