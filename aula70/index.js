6d61c1615a9da254cae3c06894337c7f1118d727
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-14 14:51:26 -0300

    aula70
    Revisão Objetos

---
+++ aula70/index.js
@@ -0,0 +1,23 @@
+// const pessoa = {
+//     nome: 'Luiz', 
+//     sobrenome: 'Otávio'
+// };
+
+// // anotação de ponto
+// console.log(pessoa.nome);
+// console.log(pessoa.sobrenome);
+
+// // anotação de colchete
+// console.log(pessoa['nome']);
+// console.log(pessoa['sobrenome']);
+
+// const chave = 'nome';
+// console.log(pessoa['nome']);
+// console.log(pessoa[chave]);
+
+const pessoa1 = new Object();
+pessoa1.nome = 'Luiz';
+pessoa1.sobrenome = 'Otávio';
+
+console.log(pessoa1.nome);
+console.log(pessoa1.sobrenome);
