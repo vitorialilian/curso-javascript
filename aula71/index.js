8779568fd9a1eddf3a715b87d6d09bdc4a33e5c1
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-14 19:11:21 -0300

    update aula71

--- aula71/index.js
+++ aula71/index.js
@@ -1,23 +1,3 @@
-function Produto(nome, preco, estoque) {
-    this.nome = nome;
-    this.preco = preco;
-    this.estoque = estoque;
-
-    Object.defineProperty(this, 'estoque', {
-        enumerable: true, // mostra a chave quando é true
-        value: estoque, // valor, neste caso vindo do parametro
-        writable: true, // pode alterar o valor se true, ou não se false
-        configurable: false // configurável se true, não permite apagar nem reconfigurar a variavel se false
-
-        // se writable false e configurable true, é possivel deletar, mas vice-versão não
-    });
-}
-
-const produto1 = new Produto('Camiseta', 20, 3);
-produto1.estoque = 500000;
-delete produto1.estoque;
-console.log(produto1);
-
 // function Produto(nome, preco, estoque) {
 //     this.nome = nome;
 //     this.preco = preco;
@@ -27,7 +7,9 @@ console.log(produto1);
 //         enumerable: true, // mostra a chave quando é true
 //         value: estoque, // valor, neste caso vindo do parametro
 //         writable: true, // pode alterar o valor se true, ou não se false
-//         configurable: true // configurável se true, não permite apagar nem reconfigurar a variavel se false
+//         configurable: false // configurável se true, não permite apagar nem reconfigurar a variavel se false
+
+//         // se writable false e configurable true, é possivel deletar, mas vice-versão não
 //     });
 // }
 
@@ -35,3 +17,74 @@ console.log(produto1);
 // produto1.estoque = 500000;
 // delete produto1.estoque;
 // console.log(produto1);
+
+// function Produto(nome, preco, estoque) {
+//     this.nome = nome;
+//     this.preco = preco;
+//     this.estoque = estoque;
+
+//     Object.defineProperty(this, 'estoque', {
+//         enumerable: true, // mostra a chave
+//         value: function () {
+//             return estoque
+//         },   // valor
+//         writable: false,  // se pode ou não alterar o valor
+//         configurable: true // se é ou não configurável
+//     });
+// }
+
+// const produto1 = new Produto('Camiseta', 20, 3);
+// produto1.estoque = 500000;
+// //delete produto1.estoque;
+// console.log(produto1.estoque());
+
+
+// function Produto(nome, preco, estoque) {
+//     this.nome = nome;
+//     this.preco = preco;
+
+//     Object.defineProperty(this, 'estoque', {
+//         enumerable: false, // true mostraria 
+//         value: estoque,
+//         writable: false,  
+//         configurable: true 
+//     });
+// }
+
+// const produto1 = new Produto('Camiseta', 20, 3);
+// console.log(Object.keys(produto1));
+
+// for (let chave in produto1) {
+//     console.log(chave);
+// }
+
+function Produto(nome, preco, estoque) {
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        value: estoque,
+        writable: false,
+        configurable: true
+    });
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable: true,
+            configurable: true
+        },
+        preco: {
+            enumerable: true,
+            value: preco,
+            writable: true,
+            configurable: true
+        },
+    });
+}
+
+const produto1 = new Produto('Camiseta', 20, 3);
+console.log(produto1);
+
+for (let chave in produto1) {
+    console.log(chave);
+}
