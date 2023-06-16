c6d14347bb0dcbeaa5291dde8b940c3ce521ec2a
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-16 15:02:49 -0300

    aula76
    Herança

---
+++ aula76/aula76.js
@@ -0,0 +1,44 @@
+// Produto -> aumento, desconto
+// Camiseta = cor, caneca = material
+
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+};
+
+Produto.prototype.aumento = function (quantia) {
+    this.preco += quantia;
+};
+
+Produto.prototype.desconto = function (quantia) {
+    this.preco -= quantia;
+};
+
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco);
+    this.cor = cor;
+}
+
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+
+Camiseta.prototype.aumento = function (percentual) {
+    this.preco = this.preco + (this.preco * percentual / 100);
+}
+
+function Caneca(nome, preco, material, estoque) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+    this.estoque = estoque;
+}
+
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+const produto = new Produto('Genérico', 111)
+const camiseta = new Camiseta('Regata', 7.5, 'Preta');
+const caneca = new Caneca('Xircrão', 15, 'Porcelana');
+caneca.aumento(15);
+console.log(produto);
+console.log(camiseta);
+console.log(caneca);
