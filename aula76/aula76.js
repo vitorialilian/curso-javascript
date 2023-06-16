52a2e660eb4ae8568b90dfba051a317273fbab5b
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-16 15:53:00 -0300

    Update aula76

--- aula76/aula76.js
+++ aula76/aula76.js
@@ -29,16 +29,30 @@ Camiseta.prototype.aumento = function (percentual) {
 function Caneca(nome, preco, material, estoque) {
     Produto.call(this, nome, preco);
     this.material = material;
-    this.estoque = estoque;
+    
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: false,
+        get: function() {
+            return estoque;
+        },
+        set: function(valor) {
+            if (typeof valor !== 'number') return;
+            estoque = valor;
+        }
+    });
 }
 
 Caneca.prototype = Object.create(Produto.prototype);
 Caneca.prototype.constructor = Caneca;
 
-const produto = new Produto('Genérico', 111)
+
+const produto = new Produto('Genérico', 111);
 const camiseta = new Camiseta('Regata', 7.5, 'Preta');
-const caneca = new Caneca('Xircrão', 15, 'Porcelana');
-caneca.aumento(15);
-console.log(produto);
-console.log(camiseta);
+const caneca = new Caneca('Xircrão', 15, 'Porcelana', 6);
+caneca.estoque = 100; // setter
+
+console.log(caneca.estoque); // getter
 console.log(caneca);
+console.log(camiseta);
+console.log(produto);
