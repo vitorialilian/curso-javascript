f0be03ed833c8c468dca3f0936b1ea2810a2d09c
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-16 17:35:17 -0300

    Update aula77

--- aula77/aula77.js
+++ aula77/aula77.js
@@ -1,5 +1,7 @@
-// 705.484.450-52  070.987.720-03
-/*
+/* Algoritmo
+705.484.450-52  070.987.720-03
+
+
 7x 0x 5x 4x 8x 4x 4x 5x 0x 
 10 9  8  7  6  5  4  3  2
 70 0  40 28 48 20 16 15 0 = 237
@@ -17,10 +19,16 @@ Se o número dígito for maior que 9, consideramos 0.
 
 let cpf = '705.484.450-52';
 let cpfLimpo = cpf.replace(/\D+/g, '');
-cpfArray = Array.from(cpfLimpo);
+const cpfArray = Array.from(cpfLimpo);
 
-console.log(cpf);
-console.log(cpfLimpo);
-console.log(cpfArray.map(el => el));
-console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0));
+const array = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+
+
+
+// console.log(cpf);
+// console.log(cpfLimpo);
+// console.log(cpfArray.map(el => el));
+// console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0));
+
+console.log(cpfArray[0] * array[0]);
 
