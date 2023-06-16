e16bb47ef21f7cb3a20d11e639001bbdbef7e232
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-16 20:56:12 -0300

    Update aula77

--- aula77/aula77.js
+++ aula77/aula77.js
@@ -17,18 +17,49 @@ Se o número dígito for maior que 9, consideramos 0.
 Se o número dígito for maior que 9, consideramos 0.
 */
 
-let cpf = '705.484.450-52';
-let cpfLimpo = cpf.replace(/\D+/g, '');
-const cpfArray = Array.from(cpfLimpo);
+function ValidarCPF(cpfEnviado) {
+   Object.defineProperty(this, 'cpfLimpo', {
+    get: function() {
+        return cpfEnviado.replace(/\D+/g, '');
+    }
+   });
+}
 
-const array = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+ValidarCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
 
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
 
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
+};
 
-// console.log(cpf);
-// console.log(cpfLimpo);
-// console.log(cpfArray.map(el => el));
-// console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0));
+ValidarCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
 
-console.log(cpfArray[0] * array[0]);
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((acumulador, valor) => {
+        acumulador += (regressivo * Number(valor));
+        regressivo--;
+        return acumulador;
+    }, 0);
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito); 
+}
 
+ValidarCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo; 
+};
+
+const cpf = new ValidarCPF('069.374.833-84');
+
+if(cpf.valida()) {
+    console.log('Cpf Válido');
+} else {
+    console.log('Cpf Inválido');
+}
