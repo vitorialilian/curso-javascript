3b05c53cca5b854e0a11b2c6739083a45499a8b0
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-18 16:35:15 -0300

    Update aula78

--- aula78/aula78.js
+++ aula78/aula78.js
@@ -7,6 +7,28 @@ function Conta(agencia, conta, saldo) {
     this.saldo = saldo;
 }
 
+Conta.prototype.sacar = function (valor) {
+    if (valor > (this.saldo + this.limite)) {
+        console.log(`Saldo insuficiente: ${this.saldo}`);
+        this.verSaldo();
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+Conta.prototype.depositar = function (valor) {
+    this.saldo += valor;
+    this.verSaldo();
+};
+Conta.prototype.verSaldo = function () {
+    console.log(
+        `Agência/conta: ${this.agencia}/${this.conta} | ` +
+        `Saldo:R$ ${this.saldo.toFixed(2)}`
+    );
+};
+
 function ContaCorrente(agencia, conta, saldo, limite) {
     Conta.call(this, agencia, conta, saldo);
     this.limite = limite;
@@ -15,30 +37,34 @@ function ContaCorrente(agencia, conta, saldo, limite) {
 ContaCorrente.prototype = Object.create(Conta.prototype);
 ContaCorrente.prototype.constructor = ContaCorrente;
 
-ContaCorrente.prototype.sacar = function(valor) {
-    if(valor > (this.saldo + this.limite)) {
+ContaCorrente.prototype.sacar = function (valor) {
+    if (valor > (this.saldo + this.limite)) {
         console.log(`Saldo insuficiente: ${this.saldo}`);
-        this.verSaldo();
-        return; 
+        return;
     }
 
     this.saldo -= valor;
     this.verSaldo();
 };
 
-Conta.prototype.depositar = function(valor) {
-    this.saldo += valor;
-    this.verSaldo();
-};
-Conta.prototype.verSaldo = function() {
-    console.log(
-        `Agência/conta: ${this.agencia}/${this.conta} | ` +
-        `Saldo:R$ ${this.saldo.toFixed(2)}`
-    );
-};
+function ContaPoupanca(agencia, conta, saldo) {
+    Conta.call(this, agencia, conta, saldo);
+}
+
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.prototype.constructor = ContaPoupanca;
 
 const contaCorrente1 = new ContaCorrente(11, 22, 0, 100);
-contaCorrente1.depositar(15);
-contaCorrente1.sacar(22);
-contaCorrente1.sacar(50);
+contaCorrente1.depositar(10);
+contaCorrente1.sacar(110);
+contaCorrente1.sacar(1);
+
+console.log();
+
+const contaPoupanca1 = new ContaPoupanca(12, 33, 0);
+contaPoupanca1.depositar(10);
+contaPoupanca1.sacar(110);
+contaPoupanca1.sacar(1);
+
+
 
