403bb969f6886c77c7388627917f01ba2e0ee0dd
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-18 20:48:16 -0300

    Update aula79

--- aula79/aula79.js
+++ aula79/aula79.js
@@ -1,10 +1,36 @@
 // Factory Functions + Prototypes
+const falar = {
+    falar() {
+    console.log(`${this.nome} está falando`);
+    },
+}
 
-function criaPessoa(nome,sobrenome) {
-    return {
-        nome, 
-        sobrenome
-    };
+const comer = {
+    comer() {
+    console.log(`${this.nome} está comendo`);
+    },
+}
+
+const beber = {
+    beber() {
+    console.log(`${this.nome} está bebendo`);
+    },
+}
+
+// const pessoaPrototype = { ...falar, ...beber, ...comer };
+const pessoaPrototype = Object.assign({}, falar, comer, beber);
+
+
+function criaPessoa(nome, sobrenome) {
+   return Object.create(pessoaPrototype, {
+        nome: { value: nome },
+        sobrenome: { value: sobrenome }
+    });
 }
 
 const pessoa1 = criaPessoa('Luiz', 'Otávio');
+
+const pessoa2 = criaPessoa('Maria', 'Silva')
+
+console.log(pessoa1);
+console.log(pessoa2);
