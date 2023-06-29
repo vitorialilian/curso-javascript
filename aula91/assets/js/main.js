1a46b1953925767ad40a3c83608ec85ff9dc6fe4
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-06-29 17:47:13 -0300

    aula91
    Fetch API (GET)

---
+++ aula91/assets/js/main.js
@@ -0,0 +1,56 @@
+// const request = obj => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(obj.method, obj.url, true);
+//         xhr.send();
+
+//         xhr.addEventListener('load', () => {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.responseText);
+//             } else {
+//                 reject(xhr.statusText);
+//             }
+//         });
+//     });
+// };
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+    try {
+    const href = el.getAttribute('href');
+    const response = await fetch(href);
+
+    if(response.status !== 200) throw new Error('Erro 404!');
+
+    const html = await response.text();
+    carregaResultado(html); 
+    } catch(e){
+        console.error(e);
+    }
+}
+
+// function carregaPagina(el) {
+//     const href = el.getAttribute('href');
+
+//     fetch(href)
+//         .then(response => {
+//             if(response.status !== 200) throw new Error('ERRO 404!')
+//             return response.text()
+//         })
+//         .then(html => carregaResultado(html))
+//         .catch(e => console.log(a)); 
+// }
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
