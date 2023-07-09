b8906c68c2a398e5070c5dc05c69017c408e88bb
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-07-09 19:44:58 -0300

    Update node aula01

--- node/aula01/app.js
+++ node/aula01/app.js
@@ -15,6 +15,31 @@
 // console.log(falaNome());
 
 // MODO 4 DE EXPORTAÇÃO
-const { nome, sobrenome, falaNome } = require('./mod1');
-console.log(nome, sobrenome);
-console.log(falaNome());
+// const { nome, sobrenome, falaNome } = require('./mod1');
+// console.log(nome, sobrenome);
+// console.log(falaNome());
+
+// const path = require('path');
+
+// const axios = require('axios');
+
+
+// axios('https://www.otaviomiranda.com.br/files/json/pessoas.json')
+// .then(response => console.log(response.data))
+// .catch(e => console.log(e))
+
+
+
+// const { Pessoa } = require('./mod1');
+// const p1 = new Pessoa('Luiz');
+
+// console.log(p1);
+
+const { Pessoa } = require('./mod1');
+const mod1 = require('./mod1');
+
+const p1 = new Pessoa('Jão');
+
+console.log(mod1);
+
+console.log(p1);
--- node/aula01/mod1.js
+++ node/aula01/mod1.js
@@ -1,17 +1,38 @@
+// const nome = 'Luiz';
+// const sobrenome = 'Miranda';
+
+// const falaNome = () => nome + ' ' + sobrenome;
+
+// // module.exports.nome = nome;
+// // module.exports.sobrenome = sobrenome;
+// // module.exports.falaNome = falaNome;
+
+// // console.log(module.exports);
+
+// exports.nome = nome;
+// exports.sobrenome = sobrenome;
+// exports.falaNome = falaNome;
+// this.qualquerCoisa = 'O que você quiser exportar'
+
+// // console.log(exports);
+
+// class Pessoa {
+//     constructor(nome) {
+//         this.nome = nome;
+//     }
+// }
+
+// exports.Pessoa = Pessoa;
+
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+
 const nome = 'Luiz';
 const sobrenome = 'Miranda';
 
-const falaNome = () => nome + ' ' + sobrenome;
-
-// module.exports.nome = nome;
-// module.exports.sobrenome = sobrenome;
-// module.exports.falaNome = falaNome;
-
-// console.log(module.exports);
-
-exports.nome = nome;
-exports.sobrenome = sobrenome;
-exports.falaNome = falaNome;
-this.qualquerCoisa = 'O que você quiser exportar'
-
-// console.log(exports);
+module.exports = {
+    nome, sobrenome, Pessoa
+}
