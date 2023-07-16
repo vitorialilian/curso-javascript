95709ee7c252cdeb92a4e28cac40b7c11474ec6e
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-07-16 14:11:21 -0300

    Update node aula05

--- node/aula05/app.js
+++ node/aula05/app.js
@@ -1,24 +1,25 @@
 const path = require('path');
-const caminhoArquivo = path.resolve(__dirname, '..', 'teste.json');
+const caminhoArquivo = path.resolve(__dirname,'teste.json');
 const escreve = require('./modules/escrever');
 const ler = require('./modules/ler');
 
-// const pessoas = [
-//     {nome: 'João'},
-//     {nome: 'Maria'},
-//     {nome: 'Eduardo'},
-//     {nome: 'Luiza'},
-// ];
+const pessoas = [
+    { nome: 'João' },
+    { nome: 'Maria' },
+    { nome: 'Eduardo' },
+    { nome: 'Luiza' },
+];
 
-// const json = JSON.stringify(pessoas, '', 2);
-// escreve(caminhoArquivo, json);
+const json = JSON.stringify(pessoas, '', 2);
+escreve(caminhoArquivo, json);
 
-async function lerArquivo(caminho) {
+async function leArquivo(caminho) {
     const dados = await ler(caminho);
     renderizaDados (dados);
 }
 
 function renderizaDados(dados) {
-    console.log(dados);
+    dados = JSON.parse(dados);
+    dados.forEach(val => console.log(val));
 }
-lerArquivo(caminhoArquivo);
+leArquivo(caminhoArquivo);
--- node/aula05/modules/escrever.js
+++ node/aula05/modules/escrever.js
@@ -17,5 +17,5 @@
 const fs = require('fs').promises;
 
 module.exports = (caminho, dados) => {
-    fs.writeFile(caminho, dados, { flag: 'w'});
+    fs.writeFile(caminho, dados, { flag: 'w', encoding: 'utf8'});
 };
