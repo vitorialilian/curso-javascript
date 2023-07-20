fce3191003865cf0bb8814cbe0668ff41289d76b
lilyv30 <127898471+vitorialilian@users.noreply.github.com>
2023-07-20 14:47:29 -0300

    Update

--- node/aula08/server.js
+++ node/aula08/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
     res.send('<form action="/" method="post"> Nome do cliente: <input type="text" name="nome"> <button>Enviar Formulário</button> </form>');
 });
 
-app.get('/testes/:idUsuarios', (req, res) => {
+app.get('/testes/:idUsuarios?/:parametro?', (req, res) => {
     console.log(req.params);
-    res.send(req.params.idUsuarios);
-})
+    console.log(req.query);
+    res.send(req.query.facebook);
+});
 
 app.post('/', (req, res) => {
+    console.log(req.body);
     res.send('Recebi o formulário');
 });
 
