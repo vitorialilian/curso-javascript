af66a18d50ac80d49aebe0d9bb60636e0240bae7
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2023-07-23 17:32:45 -0300

    Node aula13
    Express middlewares

---
+++ node/aula13/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const contatoController = require('./src/controllers/contatoController');
+
+function meuMidleware(req, res) {
+    
+}
+
+// Rotas da Home
+route.get('/', homeController.paginaInicial);
+route.post('/', homeController.trataPost);
+
+// Rotas de contato
+route.get('/contato', contatoController.paginaInicial);
+
+module.exports = route;
