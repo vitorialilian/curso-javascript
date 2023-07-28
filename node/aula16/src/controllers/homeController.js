4f4c974a7ac267e27c5b717e87f843692da4cb4c
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2023-07-28 17:31:59 -0300

    Upgrade node aula16

--- node/aula16/src/controllers/homeController.js
+++ node/aula16/src/controllers/homeController.js
@@ -9,6 +9,7 @@
 
 exports.paginaInicial = (req, res) => { 
     res.render('index', {
+        // titulo: 'Este será o <span style="color: red; >titulo</span> da página',
         titulo: 'Este será o título da página',
         numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     })
