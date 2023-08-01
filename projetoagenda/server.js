b6eb3a183e045e791cbfb505a8f0b2edad3a3eee
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2023-08-01 14:03:23 -0300

    projetoagenda
    criando o template HTML (ejs)

---
+++ projetoagenda/server.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose'); // é quem vai modelar nossa base de dados
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.emit('pronto');
+    })
+    .catch(e => console.log(e));
+
+const session = require('express-session');
+
+const MongoStore = require('connect-mongo');
+
+const flash = require('connect-flash');
+
+const routes = require('./routes');
+
+const path = require('path');
+
+const helmet = require('helmet')
+
+const csrf = require('csurf');
+
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+
+app.use(helmet());
+
+app.use(express.urlencoded({extended: true})); // está dizendo que podemos postar formulários dentro da nossa aplicação
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: 'mqixgshdytegfbvu',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false, 
+    saveUnitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7, 
+        httpOnly: true
+    }
+})
+
+app.use(sessionOptions);
+
+app.use(flash());
+
+app.set('views', path.resolve(__dirname, 'src', 'views')) // Aqui esta passando o caminho absoluto
+app.set('view engine', 'ejs');
+
+app.use(csrf())
+// Nossos próprios middlewares
+app.use(middlewareGlobal); 
+
+app.use(checkCsrfError);
+
+app.use(csrfMiddleware);
+
+app.use(routes);
+
+app.on('pronto', () => {
+    app.listen(8080, () => {
+        console.log('Acessar http://localhost:8080')
+        console.log('Servidor executando na porta 8080');
+    });
+});
---
+++ projetoagenda/src/controllers/homeController.js
@@ -0,0 +1,22 @@
+// const HomeModel = require('../models/HomeModel.js');
+
+// HomeModel.create({
+//     titulo: 'Outra coisa qualquer', 
+//     descricao: 'Outra descrição'
+// })
+//     .then(dados => console.log(dados))
+//     .catch(e => console.log(e));
+
+exports.paginaInicial = (req, res) => { 
+    res.render('index', {
+        // titulo: 'Este será o <span style="color: red; >titulo</span> da página',
+        titulo: 'Este será o título da página',
+        numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    })
+    return;
+};
+
+exports.trataPost = (req, res) => {
+    res.send(req.body);
+    return;
+};
