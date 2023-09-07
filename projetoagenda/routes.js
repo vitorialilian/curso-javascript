81570d11ff0e5945682ca16fc95ac5a6ca8f5b28
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2023-09-07 20:51:43 -0300

    Update routes.js

--- projetoagenda/routes.js
+++ projetoagenda/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const route = express.Router();
-
 const homeController = require('./src/controllers/homeController');
+
 const loginController = require('./src/controllers/loginController');
 
 
