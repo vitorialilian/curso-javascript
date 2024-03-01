79cb79d483fc29e2009eb333414a546fa935c6b7
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2024-03-01 17:27:20 -0300

    atualização e criação de rotas

--- projetoagenda/routes.js
+++ projetoagenda/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController');
-
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
 
+const { loginRequired } = require('./src/middlewares/middleware')
 
 // Rotas da Home
 route.get('/', homeController.index);
@@ -11,5 +13,14 @@ route.get('/', homeController.index);
 //Rotas de login 
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+// Rotas de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editeIndex);
+route.post('/contato/edit/:id', loginRequired, contatoController.edit);
+route.get('/contato/delete/:id', loginRequired, contatoController.delete);
 
 module.exports = route;
