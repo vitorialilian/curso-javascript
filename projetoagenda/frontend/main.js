a7a2e19bbd5b9c3cd048cc046684b6d26edb4c79
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2024-03-01 17:21:07 -0300

    atualização main.js

--- projetoagenda/frontend/main.js
+++ projetoagenda/frontend/main.js
@@ -1,4 +1,18 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import './assets/css/style.css';
+import Login from './modules/Login';
+import CriarContato from './modules/CriarContato';
+
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+
+const criarContato = new CriarContato('.form-cadastro-contato');
+
+login.init();
+cadastro.init()
+
+criarContato.init();
+
+
+//import './assets/css/style.css';
