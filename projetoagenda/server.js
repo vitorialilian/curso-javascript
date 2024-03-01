4b235f59eb07f89f6fbc94385c37656332f4f2df
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2024-03-01 17:29:41 -0300

    atualização do server.js configurações do banco de dados

--- projetoagenda/server.js
+++ projetoagenda/server.js
@@ -4,7 +4,12 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose'); // é quem vai modelar nossa base de dados
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTIONSTRING,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+        // useFindAndModify: false
+    })
     .then(() => {
         app.emit('pronto');
     })
@@ -20,25 +25,25 @@ const routes = require('./routes');
 
 const path = require('path');
 
-const helmet = require('helmet')
+// const helmet = require('helmet')
 
 const csrf = require('csurf');
 
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
-app.use(helmet());
+// app.use(helmet());
 
-app.use(express.urlencoded({extended: true})); // está dizendo que podemos postar formulários dentro da nossa aplicação
+app.use(express.urlencoded({ extended: true })); // está dizendo que podemos postar formulários dentro da nossa aplicação
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
     secret: 'mqixgshdytegfbvu',
     store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
-    resave: false, 
+    resave: false,
     saveUnitialized: false,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7, 
+        maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true
     }
 })
@@ -52,7 +57,7 @@ app.set('view engine', 'ejs');
 
 app.use(csrf())
 // Nossos próprios middlewares
-app.use(middlewareGlobal); 
+app.use(middlewareGlobal);
 
 app.use(checkCsrfError);
 
@@ -61,8 +66,8 @@ app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('pronto', () => {
-    app.listen(8080, () => {
-        console.log('Acessar http://localhost:8080')
-        console.log('Servidor executando na porta 8080');
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000')
+        console.log('Servidor executando na porta 3000');
     });
 });
