c8d92891ca83647ddfae6476ebc0a5470ee4e467
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2024-03-25 14:55:28 -0300

    atualização do loginModel

--- projetoagenda/src/models/LoginModel.js
+++ projetoagenda/src/models/LoginModel.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
-    titulo: { type: String, required: true },
-    descricao: String
+    email: { type: String, required: true },
+    password: { type: String, required: true }
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
@@ -10,6 +12,73 @@ const LoginModel = mongoose.model('Login', LoginSchema);
 class Login {
     constructor(body) {
         this.body = body;
+        this.errors = [];
+        this.user = null;
+    }
+
+    async login() {
+        this.valida();
+        if (this.errors.length > 0) return;
+
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if (!this.user) {
+            this.errors.push('Usuário não existe.');
+            return;
+        }
+
+        if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Senha Inválida')
+            this.user = null;
+            return;
+        };
+    }
+
+    async register() {
+        this.valida();
+        if (this.errors.length > 0) return;
+
+        await this.userExists();
+
+        if (this.errors.length > 0) return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+        this.user = await LoginModel.create(this.body);
+
+    }
+
+    async userExists() {
+        const user = await LoginModel.findOne({ email: this.body.email });
+
+        if (this.user) this.errors.push('Usuário já existe')
+    }
+
+    valida() {
+        this.cleanUp();
+
+        // validações 
+        // o e-mail precisa ser válido
+        if (!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+
+        // a senha precisa ter entre 5 e 20 caracteres
+        if (this.body.password.length < 3 || this.body.password.length > 50) {
+            this.errors.push('A senha precisa ter entre 3 e 50 caracteres');
+        }
+    }
+
+    cleanUp() {
+        for (const key in this.body) {
+            if (typeof this.body[key] !== 'string') {
+                this.body[key] = '';
+            }
+        }
+
+        this.body = {
+            email: this.body.email,
+            password: this.body.password
+        };
     }
 }
 
