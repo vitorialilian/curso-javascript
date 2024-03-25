7fb01a5d1b60d55afbec56f8abf3a8b0a78aacf6
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2024-03-25 14:27:49 -0300

    atualização dos middlewares

--- projetoagenda/src/middlewares/middleware.js
+++ projetoagenda/src/middlewares/middleware.js
@@ -1,5 +1,7 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
+    res.locals.user = req.session.user;
     next()
 };
 
@@ -20,3 +22,13 @@ exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
 }
+
+exports.loginRequired = (req, res, next) => {
+    if(!req.session.user) {
+        req.flash('errors', 'Você precisa fazer login.');
+        req.session.save(() => res.redirect('/') );
+        return;
+    }
+
+    next();
+};
